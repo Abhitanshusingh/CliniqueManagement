5b2de06524c60c2de141bf41a205b0aa05aa479f
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-05-05 19:37:36 +0530

    ADDED: Test case to check is patients is stored in file

--- src/test/java/CliniqueManagementTest.java
+++ src/test/java/CliniqueManagementTest.java
@@ -1,4 +1,5 @@
 import com.bridgelabz.model.Doctor;
+import com.bridgelabz.model.Patients;
 import com.bridgelabz.utility.CliniqueManagementServiceImp;
 import com.bridgelabz.utility.FileSystem;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -7,7 +8,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 public class CliniqueManagementTest {
 
     public static final String doctorfilePath = "./src/main/resources/Doctor.json";
+    public static final String patientfilePath = "./src/main/resources/Patient.json";
     CliniqueManagementServiceImp cliniqueManagementServiceImp;
     FileSystem fileSystem;
     ObjectMapper objectMapper;
@@ -49,6 +50,33 @@ public class CliniqueManagementTest {
             Assert.assertEquals(doctor3.getName(), data.get(3).getName());
             Assert.assertEquals(doctor4.getName(), data.get(4).getName());
             Assert.assertEquals(doctor5.getName(), data.get(5).getName());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void givenFile_whenAddPatientDelail_shouldReturnTrue() {
+        try {
+            Patients patients1 = new Patients("Amar khan", 1, 9897636572L, 27);
+            Patients patients2 = new Patients("Ali khan", 2, 9978987631L, 22);
+            Patients patients3 = new Patients("Shahil khan", 3, 7897635382L, 23);
+            Patients patients4 = new Patients("Danish khan", 4, 8947038738L, 25);
+            Patients patients5 = new Patients("Abhi singh", 5, 7004342412L, 23);
+            cliniqueManagementServiceImp.addInformation(patients1, patientfilePath);
+            cliniqueManagementServiceImp.addInformation(patients2, patientfilePath);
+            cliniqueManagementServiceImp.addInformation(patients3, patientfilePath);
+            cliniqueManagementServiceImp.addInformation(patients4, patientfilePath);
+            cliniqueManagementServiceImp.addInformation(patients5, patientfilePath);
+
+            ArrayList<Doctor> data = objectMapper
+                    .readValue(new File(patientfilePath), new TypeReference<ArrayList<Doctor>>() {
+                    });
+            Assert.assertEquals(patients1.getName(), data.get(0).getName());
+            Assert.assertEquals(patients2.getName(), data.get(1).getName());
+            Assert.assertEquals(patients3.getName(), data.get(2).getName());
+            Assert.assertEquals(patients4.getName(), data.get(3).getName());
+            Assert.assertEquals(patients5.getName(), data.get(4).getName());
         } catch (IOException e) {
             e.printStackTrace();
         }
