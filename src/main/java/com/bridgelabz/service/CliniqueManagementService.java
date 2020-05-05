14e1089f1bc39b05ec576424d0b3e373dd7f7178
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-05-05 19:10:24 +0530

    ADDED: Pojo class to store doctor information into json file

--- src/main/java/com/bridgelabz/service/CliniqueManagementService.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.service;
-
-public class CliniqueManagementService {
-    public static void main(String[] args) {
-        System.out.println("Welcom to clinique management");
-    }
-}
---
+++ src/main/java/com/bridgelabz/service/ICliniqueManagementService.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.service;
+
+public interface ICliniqueManagementService {
+    public <E> void addInformation(E data, String filePath);
+    /**
+     *      This method is used to add data
+     *      into json file.
+     */
+}
